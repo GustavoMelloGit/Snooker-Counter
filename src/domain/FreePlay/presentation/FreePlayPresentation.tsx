beb95a99fef4dd76b0c4d512913a7a9cfc082a57
Gustavo Marques de Mello <gugamello2014@yahoo.com>
2023-06-20 18:53:55 -0300

    refactor(FreePlayPresentation.tsx): extract colors array to a constant to improve readability and maintainability feat(FreePlayPresentation.tsx): add ability to remove players by clicking on them

--- src/domain/FreePlay/presentation/FreePlayPresentation.tsx
+++ src/domain/FreePlay/presentation/FreePlayPresentation.tsx
@@ -2,14 +2,16 @@ import { $, component$, useSignal } from '@builder.io/qwik';
 import type { Player } from '../model/Player';
 import styles from './FreePlayPresentation.module.css';
 
+const colors = ['#264653', '#2a9d8f', '#e9c46a', '#f4a261', '#e76f51'];
+
 const initialPlayers: Player[] = [
   {
-    color: 'red',
+    color: colors[0],
     name: 'Player 1',
     score: 0,
   },
   {
-    color: 'blue',
+    color: colors[1],
     name: 'Player 2',
     score: 0,
   },
@@ -19,7 +21,7 @@ export default component$(() => {
 
   const addPlayer = $(() => {
     const newPlayer: Player = {
-      color: 'green',
+      color: colors[count.value.length],
       name: `Player ${count.value.length + 1}`,
       score: 0,
     };
@@ -29,12 +31,15 @@ export default component$(() => {
   return (
     <div id={styles.container}>
       <div id={styles.playersContainer}>
-        {count.value.map((player) => (
+        {count.value.map((player, index) => (
           <div
             key={`${player.name},${player.color},${player.score}`}
             class={styles.player}
             style={{
               backgroundColor: player.color,
+            }}
+            onClick$={() => {
+              count.value = count.value.filter((_, i) => i !== index);
             }}
           >
             <div>
